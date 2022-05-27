cf616cd50bb447da905731d438e03e45379666ff
Sean Leong <seanleongsr@gmail.com>
2022-05-27 18:48:18 +0800

    Add filter for minimum engagement

--- twitter.py
+++ twitter.py
@@ -3,8 +3,8 @@ import snscrape.modules.twitter as sntwitter
 import itertools
 from tabulate import tabulate
 
-loc = '1.290270, 103.851959, 10km' #SG coordinates
-testlib = itertools.islice(sntwitter.TwitterSearchScraper(' geocode:"{}" lang:en'.format(loc)).get_items(),100)
+loc = '1.290270, 103.851959, 20km' #SG coordinates
+testlib = itertools.islice(sntwitter.TwitterSearchScraper(' geocode:"{}" lang:en min_retweets:5 min_faves:5'.format(loc)).get_items(),100)
 df_test = []
 for i in testlib:
     df_test.append([i.date.ctime() ,i.user.username, i.content, i.retweetCount, i.likeCount, i.lang])
