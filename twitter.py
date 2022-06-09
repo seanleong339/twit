707c280427157df8caafc9bb1c7f38db4db051a5
Sean <shaorongleong@gmail.com>
2022-06-10 00:30:01 +0800

    Add functions for different filters and searches

--- twitter.py
+++ twitter.py
@@ -3,13 +3,15 @@ import snscrape.modules.twitter as sntwitter
 import itertools
 import folium
 import os
+import time
+import re
 import concurrent.futures
 import shutil
 from tabulate import tabulate
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
-def extractData(data):
+def extractNewsData(data):
     index = data[0]
     row = data[1]
     filename = '{}.json'.format(index)
@@ -27,17 +29,64 @@ def extractData(data):
     ).add_to(mapObj)
 
     testlib = itertools.islice(sntwitter.TwitterSearchScraper(
-        'geocode:"{}" lang:en min_retweets:5 min_faves:5 '
-        .format(row['locs']))
+        'geocode:"{}" lang:en min_retweets:5 min_faves:5 filter:news {} {}'
+        .format(row['locs'], timestart, timeend))
         .get_items(), 
-        100000)
+        1)
+
+
     df_test = pd.DataFrame(
         testlib)
+
+
+    df_test['Replies'] = df_test.apply(
+        lambda row : list(itertools.islice(sntwitter.TwitterTweetScraper(
+                row.id, mode = sntwitter.TwitterTweetScraperMode.RECURSE)
+                .get_items(),
+                100))[1:],
+        axis=1)
+
     df_test.to_json('tweets/{}'.format(filename), orient='records', date_format='iso',
                     force_ascii=False, lines=True, indent=4)
 
     file.SetContentFile('tweets/{}'.format(filename))
     file.Upload()
+
+def extractUserTweets(user):
+
+    filename = "{}'s Tweets.json".format(user)
+    file = drive.CreateFile({'title': filename})
+
+    testlib = sntwitter.TwitterSearchScraper(
+        'from:{} since:{} until:{}'
+        .format(user, timestart, timeend)).get_items() 
+
+    df_test = pd.DataFrame(
+        testlib)
+
+    df_test.to_json('tweets/{}'.format(filename), orient='records', date_format='iso',
+                    force_ascii=False, lines=True, indent=4)
+
+    file.SetContentFile('tweets/{}'.format(filename))
+    file.Upload()
+
+def replyToTweet(id):
+    filename = "Reply to tweet {}.json".format(id)
+    file = drive.CreateFile({'title': filename})
+
+    testlib = sntwitter.TwitterTweetScraper(
+                id, 
+                mode = sntwitter.TwitterTweetScraperMode.RECURSE).get_items()
+
+    df_test = pd.DataFrame(
+        testlib)
+
+    df_test.to_json('tweets/{}'.format(filename), orient='records', date_format='iso',
+                    force_ascii=False, lines=True, indent=4)
+
+    file.SetContentFile('tweets/{}'.format(filename))
+    file.Upload()
+
 
 # exception handler
 def handler(func, path, exc_info):
@@ -60,27 +109,60 @@ gauth.SaveCredentialsFile("mycreds.txt")
 drive = GoogleDrive(gauth)
 
 #import location
-data = pd.read_excel(r'C:\Users\seang\OneDrive\Desktop\Intern\twitter\location.xlsx')
+data = pd.read_excel(r'D:\Sean\Intern\Twitter\twit\location.xlsx')
 location = pd.DataFrame(data)
 location["locs"] = location['Latitude'].map(str) + ', ' + location['Longtitude'].map(str) + ', ' + location['Radius'].map(str) + 'km'
 location.set_index('Location',inplace=True)
 
-mapObj = folium.Map(location=location.loc['Singapore','Latitude':'Longtitude'])
-
 if not os.path.exists('tweets/'):
     os.mkdir('tweets/')
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
-    executor.map(extractData, location.iterrows())
-    print('Uploading tweets...')
-    executor.shutdown(wait=True)
+datereg = '^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$'
+# input time range that tweets are from, in yyyy-mm-dd format
+timestart = input('Start of time range in yyyy-mm-dd format:') #inclusive
 
-print('All tweets uploaded')
+if not timestart or not re.match(datereg,timestart):
+    timestart = ''
+else:
+    timestart = 'since:'+timestart
+timeend = input('End of time range in yyyy-mm-dd format:') #not inclusive
+if not timeend or not re.match(datereg, timeend):
+    timeend = ''
+else: 
+    timeend = 'until:' + timeend
 
-mapObj.save('output.html')
-file = drive.CreateFile({'title': 'map.html'})
-file.SetContentFile('output.html')
-file.Upload()
+first = input('Search tweets enter 1, search replies enter 2: ')
+while first != '1' and first != '2':
+    first = input('Invalid input. Enter 1 to search tweet, enter 2 to search replies: ')
+
+if first == '1':
+    second = input('Search by location enter 1, search by user enter 2: ')
+    while second != '1' and second != '2':
+        second = input('Invalid input. Enter 1 to search by location, enter 2 to search by user: ')
+    if second == '1':
+        tic = time.perf_counter()
+        mapObj = folium.Map(location=location.loc['Singapore','Latitude':'Longtitude'])
+        with concurrent.futures.ThreadPoolExecutor(max_workers=16) as executor:
+            executor.map(extractNewsData, location.iterrows())
+            print('Uploading tweets...')
+            executor.shutdown(wait=True)
+        mapObj.save('output.html')
+        file = drive.CreateFile({'title': 'map.html'})
+        file.SetContentFile('output.html')
+        file.Upload()
+        toc = time.perf_counter()
+        print(f"Downloaded the tweets in {toc - tic:0.4f} seconds")
+        print('All tweets uploaded')
+    else:
+        user = input('Enter username to search tweets:\n')
+        tic = time.perf_counter()
+        extractUserTweets(user)
+        toc = time.perf_counter()
+        print(f"Downloaded the tweets in {toc - tic:0.4f} seconds")
+        print('All tweets uploaded')
+else:
+    tweetid = input('Enter id of tweet to search replies: ')
+    replyToTweet(tweetid)
 
 #os.remove('output.html')
 shutil.rmtree('tweets/', onerror=handler)
